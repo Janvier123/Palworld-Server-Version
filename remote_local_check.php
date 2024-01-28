ee9cbcd62467cdc89033066251c92ff2dbdad95e
janvier123 <Janvier123@users.noreply.github.com>
2024-01-28 12:07:17 +0100

    Create remote_local_check.php
    first upload

---
+++ remote_local_check.php
@@ -0,0 +1,53 @@
+<?php
+error_reporting(E_ALL);
+include('vdfparser.php');
+
+$remote_version = getVersion();
+
+// 2394010 = Palworld Dedicated Server
+// windows path
+$kv = VDFParse("C:\\steamcmd\\steamapps\\appmanifest_2394010.acf");
+
+
+$local_version = $kv['AppState']['buildid'];
+
+
+if($local_version === $remote_version)
+{
+	echo "Your version is up to date! No need to update";
+}
+else
+{
+	if($local_version > $remote_version)
+	{
+		echo "Error, your version is newer then the ";
+	}
+	else if($local_version < $remote_version)
+	{
+		echo "Error, your version is newer then the remote version";
+	}	
+	else
+	{
+		echo "Error in versions";
+	}
+}
+	
+	
+function getVersion()
+{
+	// 2394010 = Palworld Dedicated Server
+	$api_url = 'https://api.steamcmd.net/v1/info/2394010';
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+	curl_setopt($ch, CURLOPT_HEADER, 0);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_URL, $api_url);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);     
+	$curl_data = curl_exec($ch);
+	curl_close($ch);
+	$json_array = json_decode($curl_data, true);
+	
+	return $json_array['data']['2394010']['depots']['branches']['public']['buildid'];
+}
+?>
